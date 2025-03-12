952152e5442c0f54a268d60218dce66290b511e1
Handsol <imhandsol@gmail.com>
2025-03-12 20:57:46 +0900

    feat: 아이템 이미지 추가 및 아이템 데이터 갱신 옵션 설정(SSG - 24시간마다 갱신)

--- src/app/items/page.tsx
+++ src/app/items/page.tsx
@@ -1,8 +1,10 @@
 import { Item } from '@/types/Item';
 import { ITEM_DATA_URL } from '../api/riot.api';
+import Image from 'next/image';
 
 const fetchItems = async () => {
-  const response = await fetch(ITEM_DATA_URL);
+  // 24시간마다 정보 갱신 옵션
+  const response = await fetch(ITEM_DATA_URL, { next: { revalidate: 86400 } });
   const jsonData = await response.json();
   return Object.values(jsonData.data) as Item[];
 };
@@ -14,6 +16,12 @@ const ItemListPage = async () => {
     <div>
       {items.map((item, index) => (
         <div key={index}>
+          <Image
+            src={`https://ddragon.leagueoflegends.com/cdn/15.5.1/img/item/${item.image.full}`}
+            alt={item.name}
+            width={30}
+            height={30}
+          />
           <p>{item.name}</p>
           <p>{item.plaintext}</p>
         </div>
