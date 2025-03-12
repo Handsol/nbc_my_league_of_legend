e27aa6f215614e8a0225cd0c2ba3c66721b687b2
Handsol <imhandsol@gmail.com>
2025-03-13 00:33:09 +0900

    feat: 챔피언 리스트에 이미지 추가

--- src/app/champions/page.tsx
+++ src/app/champions/page.tsx
@@ -1,5 +1,6 @@
 import { Champion } from '@/types/Champion';
 import { CHAMPION_DATA_URL } from '../api/riot.api';
+import Image from 'next/image';
 
 const fetchChampions = async () => {
   const response = await fetch(CHAMPION_DATA_URL);
@@ -16,6 +17,12 @@ const championsListPage = async () => {
       <div className="grid grid-cols-4 gap-4">
         {champions.map((champion, index) => (
           <div key={index} className="flex flex-col justify-center items-center w-[250px] h-[200px] border-1 gap-3">
+            <Image
+              src={`https://ddragon.leagueoflegends.com/cdn/15.5.1/img/champion/${champion.image.full}`}
+              alt={champion.name}
+              width={30}
+              height={30}
+            />
             <p>{champion.name}</p>
             <p>{champion.title}</p>
           </div>
