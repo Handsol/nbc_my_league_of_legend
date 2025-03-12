dbaaee32a8a303a8463d097d0d6c62e8c47dcdbd
Handsol <imhandsol@gmail.com>
2025-03-13 00:31:50 +0900

    feat: 챔피언 데이터를 비동기로 가져오는 함수 추가

--- src/app/champions/page.tsx
+++ src/app/champions/page.tsx
@@ -1,5 +1,16 @@
-const championsListPage = () => {
-  return <div>championsListPage</div>;
+import { Champion } from '@/types/Champion';
+import { CHAMPION_DATA_URL } from '../api/riot.api';
+
+const fetchChampions = async () => {
+  const response = await fetch(CHAMPION_DATA_URL);
+  const jsonData = await response.json();
+  return Object.values(jsonData.data) as Champion[];
+};
+
+const championsListPage = async () => {
+  const champions = await fetchChampions();
+
+  return <div>챔피언 페이지</div>;
 };
 
 export default championsListPage;
