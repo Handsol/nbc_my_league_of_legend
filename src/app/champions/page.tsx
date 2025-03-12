1325c73d6c0282c465acef4cc793abfefe9e2d21
Handsol <imhandsol@gmail.com>
2025-03-13 00:32:47 +0900

    feat: 챔피언 리스트 페이지 레이아웃 개선 및 데이터 표시 추가

--- src/app/champions/page.tsx
+++ src/app/champions/page.tsx
@@ -10,7 +10,19 @@ const fetchChampions = async () => {
 const championsListPage = async () => {
   const champions = await fetchChampions();
 
-  return <div>챔피언 페이지</div>;
+  return (
+    <div className="flex flex-col gap-10 mt-5">
+      <h1 className="flex justify-center text-5xl">Champion List</h1>
+      <div className="grid grid-cols-4 gap-4">
+        {champions.map((champion, index) => (
+          <div key={index} className="flex flex-col justify-center items-center w-[250px] h-[200px] border-1 gap-3">
+            <p>{champion.name}</p>
+            <p>{champion.title}</p>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default championsListPage;
