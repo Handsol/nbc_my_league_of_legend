7e9c1a257ec5114377cd3e7114b5019d0ca5a854
Handsol <imhandsol@gmail.com>
2025-03-13 00:05:53 +0900

    style: 기본적인 레이아웃 스타일링

--- src/app/items/page.tsx
+++ src/app/items/page.tsx
@@ -13,19 +13,22 @@ const ItemListPage = async () => {
   const items = await fetchItems();
 
   return (
-    <div>
-      {items.map((item, index) => (
-        <div key={index}>
-          <Image
-            src={`https://ddragon.leagueoflegends.com/cdn/15.5.1/img/item/${item.image.full}`}
-            alt={item.name}
-            width={30}
-            height={30}
-          />
-          <p>{item.name}</p>
-          <p>{item.plaintext}</p>
-        </div>
-      ))}
+    <div className="flex flex-col gap-10 mt-5">
+      <h1 className="flex justify-center text-5xl">Item List</h1>
+      <div className="grid grid-cols-4 gap-4">
+        {items.map((item, index) => (
+          <div key={index} className="flex flex-col justify-center items-center w-[250px] h-[200px] border-1 gap-3">
+            <Image
+              src={`https://ddragon.leagueoflegends.com/cdn/15.5.1/img/item/${item.image.full}`}
+              alt={item.name}
+              width={30}
+              height={30}
+            />
+            <p>{item.name}</p>
+            <p>{item.plaintext}</p>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
