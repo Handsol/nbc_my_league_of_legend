11b6022755dcc7fcf62ffde1bd8678da26abdfa6
Handsol <imhandsol@gmail.com>
2025-03-14 17:31:47 +0900

    feat: 챔피언 상세페이지 구현 및 데이터 가져오기 기능 추가

--- src/app/champions/[id]/page.tsx
+++ src/app/champions/[id]/page.tsx
@@ -1,5 +1,33 @@
-const championDetailPage = () => {
-  return <div>championDetailPage</div>;
+import { CHAMPION_DETAIL_URL } from '@/app/api/riot.api';
+import { Champion } from '@/types/Champion';
+import Image from 'next/image';
+
+interface ChampionDetailPageProps {
+  params: { id: string };
+}
+
+const fetchCampionById = async (id: string) => {
+  const response = await fetch(CHAMPION_DETAIL_URL(id), { cache: 'no-store' });
+  const jsonData = await response.json();
+  return jsonData.data[id] as Champion;
+};
+
+const championDetailPage = async ({ params }: ChampionDetailPageProps) => {
+  const champion = await fetchCampionById(params.id);
+
+  return (
+    <div className="flex flex-col items-center gap-5 mt-10">
+      <h1 className="text-4xl font-bold">{champion.name}</h1>
+      <p className="text-lg italic">{champion.title}</p>
+      <Image
+        src={`https://ddragon.leagueoflegends.com/cdn/15.5.1/img/champion/${champion.image.full}`}
+        alt={champion.name}
+        width={200}
+        height={200}
+      />
+      <p className="max-w-2xl text-center">{champion.blurb}</p>
+    </div>
+  );
 };
 
 export default championDetailPage;
