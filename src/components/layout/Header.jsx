b93fc5b9451be35378c723b8120974327d5b013b
Handsol <imhandsol@gmail.com>
2025-03-14 20:04:17 +0900

    feat: 헤더 컴포넌트에 내비게이션 링크 추가

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -3,17 +3,20 @@ import Link from 'next/link';
 
 const Header = () => {
   return (
-    <header className="flex flex-col justify-center items-center">
-      <div>
-        <Link href={'/'} className="flex cursor-pointer">
-          <Image src={'/page-logo.webp'} alt="Page Logo" width={150} height={20} />
-        </Link>
+    <div>
+      <div className="flex flex-col justify-center items-center gap-3">
+        <div>
+          <Link href={'/'} className="flex justify-center cursor-pointer w-auto min-w-[400px] border-b-2">
+            <Image src={'/page-logo.webp'} alt="Page Logo" width={150} height={20} />
+          </Link>
+        </div>
+        <nav className="flex gap-10 text-xl font-bold">
+          <Link href={'/items'}>아이템</Link>
+          <Link href={'/champions'}>챔피언</Link>
+          <Link href={'/rotation'}>로테이션</Link>
+        </nav>
       </div>
-      <nav className="flex gap-10">
-        <Link href={'/items'}>아이템</Link>
-        <Link href={'/champions'}>챔피언</Link>
-      </nav>
-    </header>
+    </div>
   );
 };
 
