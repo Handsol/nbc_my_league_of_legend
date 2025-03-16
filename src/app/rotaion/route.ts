f231def5cc8d5e2915fc706eb7868e59a0481902
Handsol <imhandsol@gmail.com>
2025-03-16 15:22:52 +0900

    refactor: GET 함수의 정의 방식을 변경하여 화살표 함수로 수정

--- src/app/rotaion/route.ts
+++ src/app/rotaion/route.ts
@@ -2,7 +2,7 @@ import { NextResponse } from 'next/server';
 
 const RIOT_API_KEY = process.env.RIOT_API_KEY;
 
-export async function GET() {
+export const GET = async () => {
   const response = await fetch('https://kr.api.riotgames.com/lol/platform/v3/champion-rotations', {
     headers: {
       'X-Riot-Token': RIOT_API_KEY as string
@@ -11,4 +11,4 @@ export async function GET() {
 
   const data = await response.json();
   return NextResponse.json(data, { status: 200 });
-}
+};
