5e7bd0b15aeda0fb2819ddfb13a96f5497f263a0
Handsol <imhandsol@gmail.com>
2025-03-16 15:49:48 +0900

    feat: 리그 오브 레전드 API 호출을 위한 헤더 추가 및 URL 수정

--- src/app/api/riot.api.ts
+++ src/app/api/riot.api.ts
@@ -2,4 +2,7 @@ export const ITEM_DATA_URL = `https://ddragon.leagueoflegends.com/cdn/15.5.1/dat
 export const CHAMPION_DATA_URL = `https://ddragon.leagueoflegends.com/cdn/15.5.1/data/ko_KR/champion.json`;
 export const CHAMPION_DETAIL_URL = (id: string) =>
   `https://ddragon.leagueoflegends.com/cdn/15.5.1/data/ko_KR/champion/${id}.json`;
-export const CHAMPION_ROTATION_DATA_URL = `https://kr.api.riotgames.com/lol/platform/v3/champion-rotations?api_key=${process.env.RIOT_API_KEY}`;
+export const CHAMPION_ROTATION_DATA_URL = `https://kr.api.riotgames.com/lol/platform/v3/champion-rotations`;
+export const RIOT_API_HEADERS = {
+  'X-Riot-Token': process.env.RIOT_API_KEY as string
+};
