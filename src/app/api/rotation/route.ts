16e93c7bf2826327d68c70ed4177f799dc3a3e53
Handsol <imhandsol@gmail.com>
2025-03-18 11:45:18 +0900

    feat: 로테이션 및 챔피언 데이터 불러오기 로직 추가 및 오류 처리 개선

--- src/app/api/rotation/route.ts
+++ src/app/api/rotation/route.ts
@@ -1,11 +1,32 @@
+'use server';
+
+import { CHAMPION_DATA_URL, CHAMPION_ROTATION_DATA_URL, RIOT_API_HEADERS } from '@/utils/riot.api';
 import { NextResponse } from 'next/server';
-import { CHAMPION_ROTATION_DATA_URL, RIOT_API_HEADERS } from '@/app/api/riot.api';
 
 export const GET = async () => {
-  const response = await fetch(CHAMPION_ROTATION_DATA_URL, {
-    headers: RIOT_API_HEADERS
-  });
+  try {
+    // 서버 액션 : 로테이션 데이터
+    const rotationResponse = await fetch(CHAMPION_ROTATION_DATA_URL, {
+      headers: RIOT_API_HEADERS
+    });
 
-  const data = await response.json();
-  return NextResponse.json(data, { status: 200 });
+    if (!rotationResponse.ok) {
+      return NextResponse.json({ error: '로테이션 데이터 불러오기 실패', status: rotationResponse.status });
+    }
+
+    const rotationData = await rotationResponse.json();
+
+    // 서버 액션 : 챔피언 데이터
+    const championsResponse = await fetch(CHAMPION_DATA_URL);
+
+    if (!championsResponse.ok) {
+      return NextResponse.json({ error: '챔피언 데이터 불러오기 실패', status: championsResponse.status });
+    }
+
+    const championsData = await championsResponse.json();
+
+    return NextResponse.json({ rotationData, championData: championsData.data });
+  } catch {
+    return NextResponse.json({ error: '데이터를 불러오기 실패', status: 500 });
+  }
 };
