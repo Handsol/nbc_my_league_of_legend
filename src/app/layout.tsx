af47ba76af852d0bf2eccbed02a843370bdddbc4
Handsol <imhandsol@gmail.com>
2025-03-19 02:20:17 +0900

    feat: Providers 컴포넌트를 추가하여 레이아웃에 감싸기

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next';
 import { Geist, Geist_Mono } from 'next/font/google';
 import './globals.css';
 import Header from '../components/layout/Header';
+import { Providers } from '@/components/Providers';
 
 const geistSans = Geist({
   variable: '--font-geist-sans',
@@ -18,16 +19,14 @@ export const metadata: Metadata = {
   description: '리그오브레전드의 정보를 확인하고 내가 좋아하는 캐릭터를 골라보세요!'
 };
 
-export default function RootLayout({
-  children
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="ko" className="trancy-ko">
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased `}>
-        <Header />
-        {children}
+        <Providers>
+          <Header />
+          {children}
+        </Providers>
       </body>
     </html>
   );
