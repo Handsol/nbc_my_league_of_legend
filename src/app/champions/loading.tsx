eeec996fee08f27e02b833e40a005285db91831b
Handsol <imhandsol@gmail.com>
2025-03-19 10:57:10 +0900

    feat: Loading 컴포넌트를 화살표 함수로 변경

--- src/app/champions/loading.tsx
+++ src/app/champions/loading.tsx
@@ -1,7 +1,7 @@
-export default function Loading() {
+export const Loading = () => {
   return (
     <div className="flex justify-center items-center h-screen">
       <p className="text-xl font-bold">로딩 중...</p>
     </div>
   );
-}
+};
